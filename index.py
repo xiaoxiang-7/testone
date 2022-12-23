5d9ece7441dff410785a9463acfeb47c9f8614ca
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-23 23:07:56 +0800

    make it better

--- index.py
+++ index.py
@@ -15,14 +15,15 @@ def handle_webhook():
     # 建立 Firestore 的連接
     db = firestore.client()
 
-    # 建立回應文字
-    response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
-
     # 根據使用者輸入的關鍵字，選擇要取得的集合
     if movie_rate == "全部電影":
+        # 建立回應文字
+        response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
         movies_collection = db.collection("最新電影_全部")
         query = movies_collection.stream()
     elif movie_rate in ["動作片", "喜劇片", "愛情片", "科幻片", "恐怖片", "劇情片", "戰爭片", "紀錄片"]:
+        # 建立回應文字
+        response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
         movies_collection = db.collection("最新電影_分類")
         query = movies_collection.where("rate", "==", movie_rate).stream()
 
@@ -30,7 +31,6 @@ def handle_webhook():
     movies = list(query)
     for movie in movies:
         response_text += "\n片名：" + movie.get("text") + "\n介紹：" + movie.get("link")
-    
     # 傳回回應文字
     return make_response(jsonify({
         "fulfillmentText": response_text
