a73c412b6b7b2fef9b2baac12ba898b0fdd007c9
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-23 16:13:56 +0800

    make it better

--- index.py
+++ index.py
@@ -7,39 +7,28 @@ from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
+@app.route("/webhook3", methods=["POST"])
+def webhook3():
+    # 構建一個請求對象
     req = request.get_json(force=True)
+    # 從 json 中獲取查詢結果
     action =  req.get("queryResult").get("action")
-
-    if (action == "movie"):
-        rate =  req.get("queryResult").get("parameters").get("movie")
-        info = "您選擇的電影分類是：" + rate + "，相關電影：\n"
-        collection_ref = db.collection("最新電影_全部")
-        docs = collection_ref.get()
-        result = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if rate in dict["rate"]:
-                result += "片名：" + dict["text"] + "\n"
-                result += "分類：" + dict["rate"] + "\n\n"
-                result += "介紹網址：" + dict["link"] + "\n\n\n"
-        info += result
-    elif (action == "movie"):
-        rate =  req.get("queryResult").get("parameters").get("movie")
-        info = "您選擇的電影分類是：" + rate + "，相關電影：\n"
-        collection_ref = db.collection("最新電影_分類")
-        docs = collection_ref.get()
-        result = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if rate in dict["rate"]:
-                result += "片名：" + dict["text"] + "\n"
-                result += "分類：" + dict["rate"] + "\n\n"
-                result += "介紹網址：" + dict["link"] + "\n\n\n"
-        info += result
     
-    return make_response(jsonify({"fulfillmentText": info}))
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        if (rate == "輔12級"):
+            rate = "輔導級(未滿十二歲之兒童不得觀賞)"
+        elif (rate == "輔15級"):
+            rate = "輔導級(未滿十五歲之人不得觀賞)"
+        info = "您選擇的電影分級是：" + rate + "，相關電影：\n"
 
-#if __name__ == "__main__":
-#    app.run()
+        collection_ref = db.collection("子青電影")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
