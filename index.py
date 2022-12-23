dd9844bb7c1154ce795fc236a12ea63896a64c41
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-24 00:11:00 +0800

    make it better

--- index.py
+++ index.py
@@ -5,43 +5,40 @@ firebase_admin.initialize_app(cred)
 
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
-app = Flask(__name__) # 定義一個 Flask 應用程式
+app = Flask(__name__)
 
 @app.route("/webhook3", methods=["POST"])
 def handle_webhook():
-    # 取得 Dialogflow 中傳遞過來的參數
-    movie_rate = request.get_json()['queryResult']['parameters']['movie']
 
-    # 建立 Firestore 的連接
+    movie_rate = request.get_json()['queryResult']['parameters']['movie']
+    episode_rate = request.get_json()['queryResult']['parameters']['episode']
+
     db = firestore.client()
 
-    # 根據使用者輸入的關鍵字，選擇要取得的集合
     if movie_rate == "全部電影":
-        # 建立回應文字
         response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
         movies_collection = db.collection("最新電影_全部")
         query = movies_collection.stream()
     elif movie_rate in ["動作片", "喜劇片", "愛情片", "科幻片", "恐怖片", "劇情片", "戰爭片", "紀錄片"]:
-        # 建立回應文字
         response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
         movies_collection = db.collection("最新電影_分類")
         query = movies_collection.where("rate", "==", movie_rate).stream()
-    elif movie_rate == "全部劇集":
-        # 建立回應文字
-        response_text = "您選擇的劇集分類是：" + movie_rate + "，相關劇集："
+    elif episode_rate == "全部劇集":
+        response_text = "您選擇的劇集分類是：" + episode_rate + "，相關劇集："
         movies_collection = db.collection("最新劇集_全部")
         query = movies_collection.stream()
-    elif movie_rate in ["陸劇", "港劇", "台劇", "日劇", "韓劇", "美劇", "海外劇"]:
-        # 建立回應文字
-        response_text = "您選擇的劇集分類是：" + movie_rate + "，相關劇集："
+    elif episode_rate in ["陸劇", "港劇", "台劇", "日劇", "韓劇", "美劇", "海外劇"]:
+        response_text = "您選擇的劇集分類是：" + episode_rate + "，相關劇集："
         movies_collection = db.collection("最新劇集_分類")
-        query = movies_collection.stream()
+        query = movies_collection.where("rate", "==", episode_rate).stream()
 
-    # 取得集合中的所有文件
     movies = list(query)
     for movie in movies:
         response_text += "\n片名：" + movie.get("text") + "\n介紹：" + movie.get("link")
-    # 傳回回應文字
+    episodes = list(query)
+    for episode in episodes:
+        response_text += "\n片名：" + episode.get("text") + "\n介紹：" + episode.get("link")
+
     return make_response(jsonify({
         "fulfillmentText": response_text
     }))
