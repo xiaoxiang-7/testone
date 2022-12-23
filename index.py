5db654fd523cb17c66794ba15bb3d390b3cedb37
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-23 16:16:45 +0800

    make it better

--- index.py
+++ index.py
@@ -15,20 +15,20 @@ def webhook3():
     action =  req.get("queryResult").get("action")
     
     if (action == "rateChoice"):
-        rate =  req.get("queryResult").get("parameters").get("rate")
-        if (rate == "輔12級"):
-            rate = "輔導級(未滿十二歲之兒童不得觀賞)"
-        elif (rate == "輔15級"):
-            rate = "輔導級(未滿十五歲之人不得觀賞)"
-        info = "您選擇的電影分級是：" + rate + "，相關電影：\n"
+        rate =  req.get("queryResult").get("parameters").get("movie")
+        if (rate == "動作片"):
+            rate = "動作片"
+        elif (rate == "喜劇片"):
+            rate = "喜劇片"
+        info = "您選擇的電影分類是：" + rate + "，相關電影：\n"
 
-        collection_ref = db.collection("子青電影")
+        collection_ref = db.collection("最新電影_分類")
         docs = collection_ref.get()
         result = ""
         for doc in docs:
             dict = doc.to_dict()
             if rate in dict["rate"]:
-                result += "片名：" + dict["title"] + "\n"
-                result += "介紹：" + dict["hyperlink"] + "\n\n"
+                result += "片名：" + dict["text"] + "\n"
+                result += "介紹：" + dict["link"] + "\n\n"
         info += result
     return make_response(jsonify({"fulfillmentText": info}))
