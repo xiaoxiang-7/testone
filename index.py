4f0b07d9b3d264d287e33ac68ebe21a9b7a96fd5
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-23 20:49:32 +0800

    make it better

--- index.py
+++ index.py
@@ -8,43 +8,33 @@ from datetime import datetime, timezone, timedelta
 app = Flask(__name__) # 定義一個 Flask 應用程式
 
 @app.route("/webhook3", methods=["POST"])
-
 def handle_webhook():
     # 取得 Dialogflow 中傳遞過來的參數
-    rate = request.get_json()['queryResult']['parameters']['movie']
+    rate = request.get_json()['queryResult']['parameters']['rate']
     
-
     # 建立 Firestore 的連接
     db = firestore.client()
 
+    # 建立回應文字
+    response_text = "您選擇的分類是：" + rate + "，相關影片："
+
     if rate == "全部電影":
-        # 建立回應文字
-        response_text = "您選擇的電影分類是：" + rate + "，相關電影："
         movies_collection = db.collection("最新電影_全部")
         query = movies_collection.stream()
     elif rate in ["動作片", "喜劇片", "愛情片", "科幻片", "恐怖片", "劇情片", "戰爭片", "紀錄片"]:
-        # 建立回應文字
-        response_text = "您選擇的電影分類是：" + rate + "，相關電影："
         movies_collection = db.collection("最新電影_分類")
         query = movies_collection.where("rate", "==", rate).stream()
-
-    rate = request.get_json()['queryResult']['parameters']['episode']
-
-    if rate == "全部劇集":
-        # 建立回應文字
-        response_text = "您選擇的劇集分類是：" + rate + "，相關劇集："
+    elif rate == "全部劇集":
         series_collection = db.collection("最新劇集_全部")
         query = series_collection.stream()
     elif rate in ["陸劇", "港劇", "台劇", "日劇", "韓劇", "美劇", "海外劇"]:
-        # 建立回應文字
-        response_text = "您選擇的劇集分類是：" + rate + "，相關劇集："
         series_collection = db.collection("最新劇集_分類")
         query = series_collection.where("rate", "==", rate).stream()
 
     # 取得集合中的所有文件
     movies = list(query)
-    for rate in movies:
-        response_text += "\n片名：" + rate.get("text") + "\n介紹：" + rate.get("link")
+    for movie in movies:
+        response_text += "\n片名：" + movie.get("name") + "\n介紹：" + movie.get("description")
     
     # 傳回回應文字
     return make_response(jsonify({
