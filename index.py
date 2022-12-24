39b6c91404fa734a49f5ce07a0f6bd5e89d128a8
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-24 15:39:00 +0800

    make it better

--- index.py
+++ index.py
@@ -7,24 +7,30 @@ from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
-@app.route("/webhook3", methods=["POST"])
+@app.route('/webhook3', methods=['POST'])
 def handle_webhook():
-    rate = request.get_json()['queryResult']['parameters']['rate']
+  req = request.get_json(silent=True, force=True)
+  fulfillmentText = ''
+
+  query_result = req.get('queryResult')
+  if query_result.get('action') == 'movie':
+    movie_rate = request.get_json()['queryResult']['parameters']['movie']
     db = firestore.client()
-
-    if rate == "全部電影":
-        response_text = "您選擇的電影分類是：" + rate + "\n，相關電影："
-        movies_collection = db.collection("最新電影_全部")
-        query = movies_collection.stream()
-    elif rate in ["動作片", "喜劇片", "愛情片", "科幻片", "恐怖片", "劇情片", "戰爭片", "紀錄片"]:
-        response_text = "您選擇的電影分類是：" + rate + "\n，相關電影："
-        movies_collection = db.collection("最新電影_分類")
-        query = movies_collection.where("rate", "==", rate).stream()
-
+    response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
+    movies_collection = db.collection("最新電影_全部")
+    query = movies_collection.stream()
+  elif query_result.get('action') == 'episode':
+    episode_rate = request.get_json()['queryResult']['parameters']['episode']
+    db = firestore.client()
+    response_text = "您選擇的劇集分類是：" + episode_rate + "，相關劇集："
+    movies_collection = db.collection("最新劇集_全部")
+    query = movies_collection.stream()
+  
+  # 取得集合中的所有文件
     movies = list(query)
     for movie in movies:
-        response_text += "\n片名：" + movie.get("text") + "\n介紹：" + movie.get("link")
-
+        response_text += "\n片名：" + episode.get("text") + "\n介紹：" + episode.get("link")  
+    # 傳回回應文字
     return make_response(jsonify({
         "fulfillmentText": response_text
     }))
