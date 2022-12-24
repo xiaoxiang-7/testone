b45a588518f261b78b94b10b19d788735989b12b
xiaoxiang-7 <eric410637340@gmail.com>
2022-12-24 16:39:46 +0800

    make it better

--- index.py
+++ index.py
@@ -9,9 +9,17 @@ app = Flask(__name__)
 
 @app.route("/webhook3", methods=["POST"])
 def handle_webhook():
-    episode_rate = request.get_json()['queryResult']['parameters']['episode']
+    # Extract the 'episode' or 'movie' parameter from the request body
+    episode_rate = request.get_json().get('queryResult', {}).get('parameters', {}).get('episode')
+    movie_rate = request.get_json().get('queryResult', {}).get('parameters', {}).get('movie')
+
+    # Connect to the Cloud Firestore database
     db = firestore.client()
 
+    # Initialize the response text
+    response_text = ""
+
+    # Check the value of the 'episode' parameter
     if episode_rate == "全部劇集":
         response_text = "您選擇的劇集分類是：" + episode_rate + "，相關劇集："
         movies_collection = db.collection("最新劇集_全部")
@@ -21,7 +29,7 @@ def handle_webhook():
         movies_collection = db.collection("最新劇集_分類")
         query = movies_collection.where("rate", "==", episode_rate).stream()
     else:
-        movie_rate = request.get_json()['queryResult']['parameters']['movie']
+        # Check the value of the 'movie' parameter
         if movie_rate == "全部電影":
             response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
             movies_collection = db.collection("最新電影_全部")
@@ -30,6 +38,9 @@ def handle_webhook():
             response_text = "您選擇的電影分類是：" + movie_rate + "，相關電影："
             movies_collection = db.collection("最新電影_分類")
             query = movies_collection.where("rate", "==", movie_rate).stream()
+        # If the 'episode' and 'movie' parameters are not set or have invalid values, set the response text to an error message
+        else:
+            response_text = "對不起，您輸入的不正確。"
 
     # 取得集合中的所有文件
     movies = list(query)
